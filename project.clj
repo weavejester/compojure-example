97bb392ac691abf62a11047e9a306cd675fcb8b3
James Reeves <jreeves@weavejester.com>
2011-02-01 00:25:15 +0000

    Added CSS resource

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject compojure-example "0.1.0"
   :description "Example Compojure project"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [compojure "0.6.0-RC1"]]
-  :dev-dependencies [[lein-ring "0.2.6"]]
+                 [compojure "0.6.0-RC3"]
+                 [hiccup "0.3.2"]]
+  :dev-dependencies [[lein-ring "0.3.0"]]
   :ring {:handler compojure.example.routes/app})
--- src/compojure/example/routes.clj
+++ src/compojure/example/routes.clj
@@ -1,7 +1,22 @@
 (ns compojure.example.routes
-  (:use compojure.core)
-  (:require [compojure.route :as route]))
+  (:use compojure.core
+        [hiccup core page-helpers])
+  (:require [compojure.route :as route]
+            [compojure.handler :as handler]
+            [compojure.response :as response]))
 
-(defroutes app
-  (GET "/" [] "Hello World")
+(defn index-page []
+  (html5
+    [:head
+      [:title "Hello World"]
+      (include-css "/css/style.css")]
+    [:body
+      [:h1 "Hello World"]]))
+
+(defroutes main-routes
+  (GET "/" [] (index-page))
+  (route/resources "/")
   (route/not-found "Page not found"))
+
+(def app
+  (handler/site main-routes))
