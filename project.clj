925afe47764790683528429dd016b0f5d042e7a9
James Reeves <jreeves@weavejester.com>
2011-02-06 21:13:50 +0000

    Updated Compojure version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject compojure-example "0.1.0"
   :description "Example Compojure project"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [compojure "0.6.0-RC3"]
+                 [compojure "0.6.0-RC4"]
                  [hiccup "0.3.3"]]
   :dev-dependencies [[lein-ring "0.3.1"]]
   :ring {:handler compojure.example.routes/app})
