8ac38e31092a414bfe613452f8f4de3d4727f9bb
James Reeves <jreeves@weavejester.com>
2011-02-07 02:28:42 +0000

    Updated Compojure and Hiccup dependencies

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "Example Compojure project"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [compojure "0.6.0-RC4"]
-                 [hiccup "0.3.3"]]
-  :dev-dependencies [[lein-ring "0.3.1"]]
+                 [hiccup "0.3.4"]]
+  :dev-dependencies [[lein-ring "0.3.2"]]
   :ring {:handler compojure.example.routes/app})
